bc5d150eb601def5ee78e28edc2438907c54337e
Owen Wood <owenwood0101@icloud.com>
2024-06-09 20:57:41 -0400

    feat(root.tsx): update site description

--- src/root.tsx
+++ src/root.tsx
@@ -21,7 +21,11 @@ export default component$(() => {
         <title>Owen Wood</title>
         <meta
           name='description'
-          content='I am a student at Phillips Exeter Academy with an interest in software engineering and computer science. The focus of my work has been primarily in web development, but I also have varying levels of experience in Python, Java, C++, and Rust.'
+          content='I am a student at Phillips Exeter Academy with an interest in software
+          engineering and computer science. The majority of my projects relate
+          to robotics (control theory, computer vision, et al.) and web
+          development, but I also have varying levels of experience in game and
+          app development.'
         />
         <meta
           name='author'
